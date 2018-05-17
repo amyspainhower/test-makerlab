2187a268d200edd0aa538db3322dbd0ef41d0b13
Brock Tubre <brock.tubre@csra.com>
2018-05-17 11:38:34 -0500

    Added images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Continious Deployment Makerlab';
+  title = 'Let The Free Time Roll - Makerlab';
 }
